816abfb590669366dabae12366c54d4dbd33fa04
quentin agasse <quentin.agasse@gmail.com>
2022-03-23 16:53:48 +0100

    rails simple rbnb start

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+4.times{
+
+  flat = Flat.new(name: Faker::Company.name,
+                    address: Faker::Address.street_address,
+                    description: Faker::Lorem.paragraph(sentence_count: 3),
+                    price_per_night: rand(0..100),
+                    number_of_guests: rand(0..10))
+  flat.save
+}
